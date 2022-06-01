4b3170bbd6aa2d53075d80dfe1ed897c3781230c
inc0der <inc0der.dev@gmail.com>
2022-05-31 21:11:06 -0300

    update to work with components

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,4 @@
-package ;
+package;
 
 import haxe.ui.HaxeUIApp;
 
@@ -6,7 +6,7 @@ class Main {
     public static function main() {
         var app = new HaxeUIApp();
         app.ready(function() {
-            app.addComponent(new MainView());
+            app.addComponent(new views.MainView());
 
             app.start();
         });
---
+++ src/views/MainView.hx
@@ -0,0 +1,11 @@
+package views;
+
+import haxe.ui.containers.VBox;
+import haxe.ui.events.MouseEvent;
+
+@:build(haxe.ui.ComponentBuilder.build("assets/main/main-view.xml"))
+class MainView extends VBox {
+    public function new() {
+        super();
+    }
+}
