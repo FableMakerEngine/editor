fb4bd1bf15af2348aa695a8815227fd6118afc41
inc0der <inc0der.dev@gmail.com>
2022-06-03 11:02:51 -0300

    Add map list component

---
+++ src/components/MapList.hx
@@ -0,0 +1,17 @@
+package components;
+
+import haxe.ui.containers.VBox;
+
+@:build(haxe.ui.macros.ComponentMacros.build("../../assets/main/maplist.xml"))
+class MapList extends VBox {
+	public function new() {
+		super();
+
+		var root1 = maplist.addNode({text: "World"});
+		root1.expanded = true;
+		var child = root1.addNode({text: "Map001"});
+		var node = child.addNode({text: "ChildMap001"});
+		var node = child.addNode({text: "ChildMap002"});
+		var node = root1.addNode({text: "Map002"});
+	}
+}
