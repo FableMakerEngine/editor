eaf5f4dbb631d7c8d00884e5057142295df11c24
inc0der <inc0der.dev@gmail.com>
2022-06-03 20:33:53 -0300

    add toolkit init and change to dark theme

--- src/views/MainView.hx
+++ src/views/MainView.hx
@@ -1,11 +1,14 @@
 package views;
 
+import haxe.ui.Toolkit;
+import components.MapEditor;
 import haxe.ui.containers.VBox;
-import haxe.ui.events.MouseEvent;
 
 @:build(haxe.ui.ComponentBuilder.build("assets/main/main-view.xml"))
 class MainView extends VBox {
     public function new() {
         super();
+        Toolkit.init();
+        Toolkit.theme = "dark";
     }
 }
