9307ad6bd1ef61f6d83afc4419a93c8561183b8f
inc0der <inc0der.dev@gmail.com>
2022-06-03 20:32:27 -0300

    Fix map editor class name typo

--- src/components/MapEditor.hx
+++ src/components/MapEditor.hx
@@ -3,7 +3,7 @@ package components;
 import haxe.ui.containers.VBox;
 
 @:build(haxe.ui.macros.ComponentMacros.build("assets/main/mapeditor.xml"))
-class MApEditor extends VBox {
+class MapEditor extends VBox {
 	public function new() {
 		super();
 	}
