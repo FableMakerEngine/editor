550db072ecb15d93ae6dc13a5cb27a295391a527
inc0der <inc0der.dev@gmail.com>
2022-06-04 13:06:55 -0300

    update map list class to extend tree view

--- src/components/MapList.hx
+++ src/components/MapList.hx
@@ -5,11 +5,11 @@ import haxe.ui.containers.TreeView;
 import haxe.ui.containers.VBox;
 
 @:build(haxe.ui.macros.ComponentMacros.build("assets/main/maplist.xml"))
-class MapList extends VBox {
+class MapList extends TreeView {
 	public function new() {
 		super();
 
-		var root1 = maplist.addNode({text: "World"});
+		var root1 = addNode({text: "World"});
 		root1.expanded = true;
 		var child = root1.addNode({text: "Map001"});
 		var node = child.addNode({text: "ChildMap001"});
