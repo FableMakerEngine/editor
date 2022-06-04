ced2a34cde1fcea90094317ca00fb6281062ea00
inc0der <inc0der.dev@gmail.com>
2022-06-04 13:09:34 -0300

    add component  initializations in main view class

--- src/views/MainView.hx
+++ src/views/MainView.hx
@@ -1,14 +1,26 @@
 package views;
 
+import components.TopMenu;
+import components.StatusBar;
 import haxe.ui.Toolkit;
 import components.MapEditor;
+import components.MapList;
 import haxe.ui.containers.VBox;
 
 @:build(haxe.ui.ComponentBuilder.build("assets/main/main-view.xml"))
 class MainView extends VBox {
+    public var mapEditor : MapEditor;
+    public var mapList : MapList;
+    public var statusBar : StatusBar;
+    public var topMenu : TopMenu;
+
     public function new() {
         super();
         Toolkit.init();
         Toolkit.theme = "dark";
+        topMenu = new TopMenu();
+        mapList = new MapList();
+        mapEditor = new MapEditor();
+        statusBar = new StatusBar();
     }
 }
