b2c0be3c5b3da195a1beca5704a90a6789f3efe9
inc0der <inc0der.dev@gmail.com>
2022-06-05 21:02:32 -0300

    Add checkstyle rules and format files (#4)
    * add checkstyle
    
    * add haxe format config
    
    * Fix style issues

--- src/views/MainView.hx
+++ src/views/MainView.hx
@@ -7,20 +7,20 @@ import components.MapEditor;
 import components.MapList;
 import haxe.ui.containers.VBox;
 
-@:build(haxe.ui.ComponentBuilder.build("assets/main/main-view.xml"))
+@:build(haxe.ui.ComponentBuilder.build('assets/main/main-view.xml'))
 class MainView extends VBox {
-    public var mapEditor : MapEditor;
-    public var mapList : MapList;
-    public var statusBar : StatusBar;
-    public var topMenu : TopMenu;
+  public var mapEditor: MapEditor;
+  public var mapList: MapList;
+  public var statusBar: StatusBar;
+  public var topMenu: TopMenu;
 
-    public function new() {
-        super();
-        Toolkit.init();
-        Toolkit.theme = "dark";
-        topMenu = new TopMenu();
-        mapList = new MapList();
-        mapEditor = new MapEditor();
-        statusBar = new StatusBar();
-    }
+  public function new() {
+    super();
+    Toolkit.init();
+    Toolkit.theme = 'dark';
+    topMenu = new TopMenu();
+    mapList = new MapList();
+    mapEditor = new MapEditor();
+    statusBar = new StatusBar();
+  }
 }
