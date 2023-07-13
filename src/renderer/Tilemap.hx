06646e313b88cec2415b219e0d8b214d30d56395
inc0der <inc0der.dev@gmail.com>
2023-07-13 00:28:34 -0300

    add TileCursor for displaying position on tilemap

--- src/renderer/Tilemap.hx
+++ src/renderer/Tilemap.hx
@@ -1,24 +1,24 @@
 package renderer;
 
+import renderer.objects.TileCursor;
+
 class Tilemap extends cyclops.Scene {
   public var background: h2d.Graphics;
   public var title: h2d.Text;
   public var isRotatingLeft: Bool = true;
+  public var tileSize: Int = 32;
+  private var tileCursor: TileCursor;
 
   public function new() {
     super();
     background = new h2d.Graphics(this);
-    background.beginFill(0x000000);
-    background.drawRect(0, 0, width, height);
-    background.endFill();
+    tileCursor = new TileCursor(this, tileSize, tileSize);
 
     var font: h2d.Font = hxd.res.DefaultFont.get();
     font.resizeTo(62);
     title = new h2d.Text(font);
     title.text = 'FABLE MAKER!';
     title.textAlign = Center;
-    title.x = width / 2;
-    title.y = height / 2;
     addChild(title);
   }
 
@@ -31,9 +31,18 @@ class Tilemap extends cyclops.Scene {
   }
 
   function onResized() {
+    background.clear();
     background.beginFill(0x000000);
     background.drawRect(0, 0, width, height);
     background.endFill();
+    interaction.width = width;
+    interaction.height = height;
+  }
+
+  public override function onMove(e: hxd.Event) {
+    var x = Math.floor(e.relX / tileSize) * tileSize;
+    var y = Math.floor(e.relY / tileSize) * tileSize;
+    tileCursor.setPosition(x, y);
   }
 
   public override function update(dt: Float) {
