0354b8d135618218bad2286d3e0f872db930037a
inc0der <inc0der.dev@gmail.com>
2023-07-13 19:44:11 -0300

    remove test text

--- src/renderer/Tilemap.hx
+++ src/renderer/Tilemap.hx
@@ -5,7 +5,6 @@ import renderer.objects.TileCursor;
 class Tilemap extends cyclops.Scene {
   public var parentView: haxe.ui.core.Component;
   public var background: h2d.Graphics;
-  public var title: h2d.Text;
   public var isRotatingLeft: Bool = true;
   public var tileSize: Int = 32;
   private var tileCursor: TileCursor;
@@ -17,13 +16,6 @@ class Tilemap extends cyclops.Scene {
     if (parentView != null) {
       this.parentView = parentView;
     }
-
-    var font: h2d.Font = hxd.res.DefaultFont.get();
-    font.resizeTo(62);
-    title = new h2d.Text(font);
-    title.text = 'FABLE MAKER!';
-    title.textAlign = Center;
-    addChild(title);
     resize(50, 50);
   }
 
@@ -34,8 +26,6 @@ class Tilemap extends cyclops.Scene {
   }
 
   function onResized() {
-    title.x = width / 2;
-    title.y = height / 2;
     if (parentView != null) {
       parentView.width = width;
       parentView.height = height;
@@ -55,18 +45,5 @@ class Tilemap extends cyclops.Scene {
   }
 
   public override function update(dt: Float) {
-    if (title != null) {
-      if (isRotatingLeft) {
-        title.rotate(0.2 * dt);
-      } else {
-        title.rotation -= 0.2 * dt;
-      }
-      if (title.rotation >= 0.5) {
-        isRotatingLeft = false;
-      }
-      if (title.rotation <= -0.5) {
-        isRotatingLeft = true;
-      }
-    }
   }
 }
