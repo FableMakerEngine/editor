b68691df7d0bfa2fcbc336b3c391daa0f21c66d6
inc0der <inc0der.dev@gmail.com>
2023-07-14 11:29:39 -0300

    add test tilemap

--- src/renderer/Tilemap.hx
+++ src/renderer/Tilemap.hx
@@ -1,22 +1,46 @@
 package renderer;
 
+import sys.FileSystem;
+import cyclops.tilemap.ITilemapConfig;
+import cyclops.Utils;
+import haxe.Json;
+import cyclops.tilemap.Tilemap.TileMap;
 import renderer.objects.TileCursor;
 
 class Tilemap extends cyclops.Scene {
   public var parentView: haxe.ui.core.Component;
   public var background: h2d.Graphics;
+  public var tilemap: TileMap;
   public var isRotatingLeft: Bool = true;
   public var tileSize: Int = 32;
   private var tileCursor: TileCursor;
 
   public function new(?parentView) {
     super();
-    background = new h2d.Graphics(this);
-    tileCursor = new TileCursor(this, tileSize, tileSize);
     if (parentView != null) {
       this.parentView = parentView;
     }
-    resize(50, 50);
+    initialize();
+  }
+
+  private function initialize() {
+    background = new h2d.Graphics(this);
+    createTilemap();
+    tileCursor = new TileCursor(this, tileSize, tileSize);
+    resize(20, 15);
+  }
+
+  public function createTilemap() {
+    hxd.Res.loader = new hxd.res.Loader(hxd.fs.EmbedFileSystem.create('res'));
+    var data = hxd.Res.loader.load('data/maps/Map1.json');
+    var mapData = Json.parse(data.toText());
+    var parsedData: Dynamic = Utils.parseLdtkData(mapData);
+    var config: ITilemapConfig = {
+      tilesets: parsedData.tilesets,
+      layers: parsedData.layers,
+      level: parsedData.levels[0]
+    }
+    tilemap = new TileMap(this, config);
   }
 
   public function resize(tilesX, tilesY) {
