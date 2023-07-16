8951d43207e84b95f42d121ca8a5771ef539168a
inc0der <inc0der.dev@gmail.com>
2023-07-15 20:32:52 -0400

    Update and merge the map editor and map list component (#9)
    * change mainview to workspace and add window list
    
    * move mapList and the split view to the mapeditor component
    
    * fix update method for mapeditor
    
    * fix scroll view not displaying scrollbar due to incorrect width
    
    * remove window system code as it's not required right now

--- src/views/MainView.hx
+++ src/views/MainView.hx
@@ -1,23 +1,17 @@
 package views;
 
-import haxe.ui.containers.windows.WindowManager;
 import haxe.ui.containers.VBox;
-import components.MapEditor;
 
 @:build(haxe.ui.ComponentBuilder.build('assets/main/main-view.xml'))
 class MainView extends VBox {
 
   public function new() {
     super();
-    WindowManager.instance.container = mainView;
   }
 
   public function update(dt) {
-    for (component in splitView.children) {
-      if (Std.isOfType(component, MapEditor)) {
-        var mapEditor = cast(component, MapEditor);
-        mapEditor.update(dt);
-      }
+    if (mapEditor != null) {
+      mapEditor.update(dt);
     }
   }
 }
