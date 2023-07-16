5a455d4370b9120f6d40dffb1814a8a6a574b778
inc0der <inc0der.dev@gmail.com>
2023-07-16 13:58:38 -0300

    add base tile picker component

---
+++ src/components/TilePicker.hx
@@ -0,0 +1,10 @@
+package components;
+
+import haxe.ui.containers.VBox;
+
+@:build(haxe.ui.ComponentBuilder.build('assets/main/tile-picker.xml'))
+class TilePicker extends VBox {
+  public function new() {
+    super();
+  }
+}
