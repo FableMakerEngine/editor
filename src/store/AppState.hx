8cad93b62a83049b65bb1da1684b135815b6cab5
inc0der <inc0der.dev@gmail.com>
2023-08-01 18:12:49 -0300

    update to set default value for state props

--- src/store/AppState.hx
+++ src/store/AppState.hx
@@ -3,7 +3,7 @@ package store;
 import tracker.Observable;
 
 class AppState implements Observable implements ReadOnly {
-  @observe public var projectPath: String;
-  @observe public var recentlyOpenedProjects: Array<String>;
+  @observe public var projectPath: String = '';
+  @observe public var recentlyOpenedProjects: Array<String> = [];
   public function new() {}
 }
