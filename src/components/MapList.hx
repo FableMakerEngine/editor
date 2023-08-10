3c0c890c4b6c03fd1dd1d01753351a628b33c46e
inc0der <inc0der.dev@gmail.com>
2023-08-10 17:31:47 -0300

    add logic to load map info xml and build nodes recursively

--- src/components/MapList.hx
+++ src/components/MapList.hx
@@ -1,5 +1,6 @@
 package components;
 
+import ceramic.Files;
 import haxe.ui.events.MouseEvent;
 import haxe.ui.containers.TreeViewNode;
 import haxe.ui.containers.TreeView;
@@ -17,6 +18,49 @@ class MapList extends TreeView {
     contextMenu.items = menu();
     worldNode = addNode({ text: 'World' });
     worldNode.expanded = true;
+    store.state.onProjectPathChange(null, onProjectPathChanged);
+  }
+
+  public function onProjectPathChanged(newPath, prevPath) {
+    var dataPath = store.state.dataDir;
+    var mapInfoPath = '$dataPath\\MapInfo.xml';
+
+    if (Files.exists(mapInfoPath)) {
+      var mapInfo = Files.getContent('$dataPath\\MapInfo.xml');
+      var data = Xml.parse(mapInfo);
+      createNodesFromXml(data.firstElement());
+    } else {
+      trace('unable to find the MapInfo.xml');
+    }
+  }
+
+  private function createNodesFromXml(xmlData: Xml, ?parentNode: TreeViewNode) {
+    var maps = xmlData.elements();
+
+    if (parentNode == null) {
+      parentNode = worldNode;
+    }
+
+    for (map in maps) {
+      var name = map.get('name');
+      var children = map.elements();
+      var node = parentNode.addNode({ text: name });
+
+      // We only expand as a temp fix for a bug in the ceramic backend of haxeui
+      node.expanded = true;
+
+      if (children.hasNext()) {
+        createNodesFromXml(map, node);
+      }
+    }
+  }
+
+  private function createNode(parent, data: Dynamic): TreeViewNode {
+    var node = new TreeViewNode();
+    node.data = { text: data.text };
+    node.parentNode = parent;
+    parent.addComponent(node);
+    return node;
   }
 
   public function menu(): Array<ContextMenuEntry> {
