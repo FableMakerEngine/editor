b05a860310ade8668c3eb9f471c0c36d728114e7
inc0der <inc0der.dev@gmail.com>
2023-08-10 17:30:59 -0300

    add data and asset dir props to state

--- src/store/AppState.hx
+++ src/store/AppState.hx
@@ -5,7 +5,19 @@ import tracker.Observable;
 class AppState implements ReadOnly implements Observable {
   @observe public var projectPath: String = '';
   @observe public var recentlyOpenedProjects: Array<String> = [];
+  public var dataDir(get, null): String;
+  public var assetsDir(get, null): String;
+
   public function new() {}
+
+  public function get_dataDir() {
+    return '$projectPath\\data\\';
+  }
+
+  public function get_assetsDir() {
+    return '$projectPath\\assets\\';
+  }
+
   public function serializeableData() {
     macros.GettersToObject.create();
   }
