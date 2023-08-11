9ecb4c418b47ca561e4c96ad28873507914d691c
inc0der <inc0der.dev@gmail.com>
2023-08-11 10:56:01 -0300

    Add MapList loading (#18)
    * add sample MapInfo xml
    
    * add data and asset dir props to state
    
    * add logic to load map info xml and build nodes recursively
    
    * move test map to sample project
    
    * fix issue where project path would not call listeners upon app load
    
    * fix getter methods visibility
    
    * add removal of child nodes of the world in onProjectPathChanged
    
    * remove useless method
    
    * remove useless property
    
    * update to use mapInfoPath when loading file

--- src/components/MapList.hx
+++ src/components/MapList.hx
@@ -1,5 +1,6 @@
 package components;
 
+import ceramic.Files;
 import haxe.ui.events.MouseEvent;
 import haxe.ui.containers.TreeViewNode;
 import haxe.ui.containers.TreeView;
@@ -17,6 +18,54 @@ class MapList extends TreeView {
     contextMenu.items = menu();
     worldNode = addNode({ text: 'World' });
     worldNode.expanded = true;
+  }
+
+  public override function onReady() {
+    store.state.onProjectPathChange(null, onProjectPathChanged);
+  }
+
+  public function onProjectPathChanged(newPath, prevPath) {
+    var dataPath = store.state.dataDir;
+    var mapInfoPath = '$dataPath\\MapInfo.xml';
+
+    if (Files.exists(mapInfoPath)) {
+      var mapInfo = Files.getContent(mapInfoPath);
+      var data = Xml.parse(mapInfo);
+      removeAllChildNodes(worldNode);
+      createNodesFromXml(data.firstElement());
+    } else {
+      trace('unable to find the MapInfo.xml');
+    }
+  }
+
+  private function createNodesFromXml(xmlData: Xml, ?parentNode: TreeViewNode) {
+    var maps = xmlData.elements();
+
+    if (parentNode == null) {
+      parentNode = worldNode;
+    }
+
+    for (map in maps) {
+      var name = map.get('name');
+      var children = map.elements();
+      var node = parentNode.addNode({ text: name });
+
+      // We only expand as a temp fix for a bug in the ceramic backend of haxeui
+      node.expanded = true;
+
+      if (children.hasNext()) {
+        createNodesFromXml(map, node);
+      }
+    }
+  }
+
+  private function removeAllChildNodes(parent: TreeViewNode) {
+    var children = parent.childComponents;
+    for (child in children) {
+      if (Std.isOfType(child, TreeViewNode)) {
+        parent.removeNode(cast child);
+      }
+    }
   }
 
   public function menu(): Array<ContextMenuEntry> {
--- src/views/MainView.hx
+++ src/views/MainView.hx
@@ -5,15 +5,18 @@ import haxe.ui.containers.VBox;
 
 @:build(haxe.ui.ComponentBuilder.build('../../assets/main/main-view.xml'))
 class MainView extends VBox {
-
   public function new() {
     super();
+  }
+  
+  public override function onReady() {
     store.initializeStorage();
     if (!store.storage.exists('locale')) {
       store.storage.set('locale', 'en_US');
     }
     LocaleManager.instance.language = store.storage.get('locale');
     store.state.onProjectPathChange(null, onProjectPathChanged);
+    store.state.invalidateProjectPath();
   }
   
   public function onProjectPathChanged(newPath, prevPath) {
