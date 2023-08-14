e1c9c0c3513f77953d33b34930c11d782c40d757
inc0der <inc0der.dev@gmail.com>
2023-08-14 17:57:16 -0300

    add tileset image loading when active map changes

--- src/components/TilePicker.hx
+++ src/components/TilePicker.hx
@@ -1,10 +1,34 @@
 package components;
 
+import haxe.ui.components.Image;
+import ceramic.Files;
 import haxe.ui.containers.VBox;
 
 @:build(haxe.ui.ComponentBuilder.build('../../assets/main/tile-picker.xml'))
 class TilePicker extends VBox {
   public function new() {
     super();
+    store.state.onActiveMapChange(null, onActiveMapChanged);
+  }
+
+  private function onActiveMapChanged(newMap: MapInfo, oldMap: MapInfo) {
+    var dataDir = store.state.dataDir;
+    var assetDir = store.state.assetsDir;
+    var mapFilename = newMap.path;
+    var mapPath = '$dataDir\\$mapFilename';
+    var mapData = Files.getContent(mapPath);
+    var mapXml = Xml.parse(mapData).firstElement();
+    var tilesetElements = mapXml.elementsNamed('tileset');
+    var imageSource = '';
+
+    // for testing
+    for (tileset in tilesetElements) {
+      var imageElement = tileset.firstElement();
+      imageSource = imageElement.get('source');
+    }
+    var filename = haxe.io.Path.withoutDirectory(imageSource);
+    var tilesetrPath = '$assetDir\\img\\tilesets';
+    tilesetImage.resource = 'file://$tilesetrPath\\$filename';
+    // trace(imageSource);
   }
 }
