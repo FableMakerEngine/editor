14d698b1d085705b55d066a4bb070b3b179d8bf6
inc0der <inc0der.dev@gmail.com>
2023-08-25 00:04:14 -0300

    update tilemap viewport to handle new tileSize state

--- src/renderer/TilemapViewport.hx
+++ src/renderer/TilemapViewport.hx
@@ -1,5 +1,6 @@
 package renderer;
 
+import ceramic.Rect;
 import ceramic.Color;
 import ceramic.Border;
 import ceramic.RuntimeAssets;
@@ -12,7 +13,7 @@ class TilemapViewport extends ceramic.Scene {
   public var tilemap(default, null): ceramic.Tilemap;
   public var mapCols: Int = 16;
   public var mapRows: Int = 16;
-  public var tileSize: Int = 32;
+  public var tileSize: Rect = new Rect(0, 0, 32, 32);
   public var tileCursor: ceramic.Border;
 
   private var mapPath: String;
@@ -25,6 +26,7 @@ class TilemapViewport extends ceramic.Scene {
     depth = 1;
     screen.onPointerMove(this, onPointerMove);
     store.state.onActiveMapChange(null, onActiveMapChanged);
+    store.state.onTileSizeChange(null, onTileSizeChanged);
   }
 
   private function onActiveMapChanged(newMap: MapInfo, oldMap: MapInfo) {
@@ -33,6 +35,13 @@ class TilemapViewport extends ceramic.Scene {
       return;
     }
     loadMap(newMap);
+  }
+
+  private function onTileSizeChanged(newSize: Rect, oldSize: Rect) {
+    tileSize = newSize;
+    tileCursor.size(tileSize.width, tileSize.height);
+    mapCols = Math.round(tilemap.width / tileSize.width);
+    mapRows = Math.round(tilemap.height / tileSize.height);
   }
 
   public override function preload() {}
@@ -54,7 +63,7 @@ class TilemapViewport extends ceramic.Scene {
     tileCursor = new Border();
     tileCursor.borderColor = Color.SNOW;
     tileCursor.borderSize = 2;
-    tileCursor.size(tileSize, tileSize);
+    tileCursor.size(tileSize.width, tileSize.height);
     tileCursor.depth = 99;
     add(tileCursor);
   }
@@ -65,11 +74,11 @@ class TilemapViewport extends ceramic.Scene {
   }
 
   public function mapWidth() {
-    return tileSize * mapCols;
+    return tileSize.width * mapCols;
   }
 
   public function mapHeight() {
-    return tileSize * mapRows;
+    return tileSize.height * mapRows;
   }
 
   public override function resize(width, height) {
@@ -90,8 +99,8 @@ class TilemapViewport extends ceramic.Scene {
     screenToVisual(info.x, info.y, localCoords);
 
     if (localCoords.x > 0 && localCoords.y > 0 && localCoords.x < mapWidth() && localCoords.y < mapHeight()) {
-      var x = Math.floor(localCoords.x / tileSize) * tileSize;
-      var y = Math.floor(localCoords.y / tileSize) * tileSize;
+      var x = Math.floor(localCoords.x / tileSize.width) * tileSize.width;
+      var y = Math.floor(localCoords.y / tileSize.height) * tileSize.height;
       tileCursor.pos(x, y);
     }
   }
@@ -101,19 +110,18 @@ class TilemapViewport extends ceramic.Scene {
   private function emptyTilemapData(name: String) {
     var data = new ceramic.TilemapData();
     data.name = name;
-    data.width = 20 * tileSize;
-    data.height = 20 * tileSize;
+    data.width = Math.round(20 * tileSize.width);
+    data.height = Math.round(20 * tileSize.height);
     return data;
   }
 
   // @TODO assign better default values based on tilesize? or user settings
   private function loadEmptyMap(mapInfo: MapInfo) {
-    tileSize = 16;
     tilemap.tilemapData = emptyTilemapData(mapInfo.name);
-    mapCols = Math.round(tilemap.width / tileSize);
-    mapRows = Math.round(tilemap.height / tileSize);
+    mapCols = Math.round(tilemap.width / tileSize.width);
+    mapRows = Math.round(tilemap.height / tileSize.height);
     resize(tilemap.width, tilemap.height);
-    tileCursor.size(tileSize, tileSize);
+    tileCursor.size(tileSize.width, tileSize.height);
   }
 
   private function loadMap(map: MapInfo) {
@@ -124,7 +132,6 @@ class TilemapViewport extends ceramic.Scene {
       mapCols = Math.round(tilemap.width / tileSize);
       mapRows = Math.round(tilemap.height / tileSize);
       resize(tilemap.width, tilemap.height);
-      tileCursor.size(tileSize, tileSize);
     }
   }
 }
