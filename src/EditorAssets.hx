556511bebacfd705e0024528b684360590f61d4f
inc0der <inc0der.dev@gmail.com>
2023-08-27 09:08:55 -0300

    move asset related paths to EditorAssets

--- src/EditorAssets.hx
+++ src/EditorAssets.hx
@@ -1,3 +1,4 @@
+import ceramic.Files;
 import ceramic.TilemapAsset;
 import ceramic.RuntimeAssets;
 import ceramic.Assets;
@@ -7,11 +8,24 @@ using ceramic.TilemapPlugin;
 class EditorAssets extends Assets {
   public static final instance: EditorAssets = new EditorAssets();
 
+  public var dataDir(get, null): String;
+  public var assetsDir(get, null): String;
+
+
+
   @event function tilemapDataReady();
   @event function tilemapDataError();
 
   private function new() {
     super();
+  }
+
+  private function get_dataDir() {
+    return '${runtimeAssets.path}\\data';
+  }
+
+  private function get_assetsDir() {
+    return '${runtimeAssets.path}\\assets';
   }
 
   public function setDirectory(path: String) {
--- src/store/AppState.hx
+++ src/store/AppState.hx
@@ -8,18 +8,7 @@ class AppState implements ReadOnly implements Observable {
   @observe public var activeMap: MapInfo;
   @observe public var mapTabs: Array<MapInfo>;
 
-  public var dataDir(get, null): String;
-  public var assetsDir(get, null): String;
-
   public function new() {}
-
-  private function get_dataDir() {
-    return '$projectPath\\data';
-  }
-
-  private function get_assetsDir() {
-    return '$projectPath\\assets';
-  }
 
   public function serializeableData() {
     macros.GettersToObject.create();
