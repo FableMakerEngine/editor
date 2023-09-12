bcca1fda9c32d55ace303bd8c7819ea0f8fb13ea
inc0der <inc0der.dev@gmail.com>
2023-09-12 11:36:26 -0300

    [wip] add test tilemap drawing from selected tiles

--- src/components/MapEditor.hx
+++ src/components/MapEditor.hx
@@ -1,5 +1,9 @@
 package components;
 
+import ceramic.TilemapLayerData;
+import ceramic.TilemapData;
+import ceramic.TilemapTile;
+import ceramic.TouchInfo;
 import components.menus.ContextMenu;
 import haxe.ui.events.MouseEvent;
 import components.menus.ContextMenuEntry;
@@ -10,12 +14,17 @@ import haxe.ui.containers.VBox;
 class MapEditor extends VBox {
   public var contextMenu: ContextMenu;
   public var viewport: TilemapViewport;
+  public var layerName: String;
+
+  var selectedTiles: Array<Tile>;
 
   public function new() {
     super();
     contextMenu = new ContextMenu();
     contextMenu.items = menu();
+    store.state.onSelectedTilesChange(null, onSelectedTilesChanged);
     viewport = new TilemapViewport(tileView);
+    viewport.onOnTilemapClick(null, onTilemapClick);
   }
 
   public function menu(): Array<ContextMenuEntry> {
@@ -83,6 +92,46 @@ class MapEditor extends VBox {
 
   public override function onResized() {
     viewport.resize(48 * 20, 48 * 16);
+  }
+
+  function onSelectedTilesChanged(newTiles: Array<Tile>, oldTiles: Array<Tile>) {
+    selectedTiles = newTiles;
+  }
+
+  function onTilemapClick(info: TouchInfo, tiles: Array<Tile>) {
+    var clickedTile = tiles[0];
+    var tilemap = viewport.tilemap;
+    trace(clickedTile);
+
+    // for testing
+    layerName = 'lower_ground';
+    // handle fill
+    // right click erase
+    if (info.buttonId == 0 || info.buttonId == 2) {
+      var tilemapData = tilemap.tilemapData;
+      if (tilemapData != null) {
+        var layers = tilemapData.layers;
+        var layerData = tilemapData.layer(layerName);
+        var layer = tilemap.layer(layerName);
+        if (layerData != null && layer != null) {
+          var index = clickedTile.frame;
+          var tiles = [].concat(layerData.tiles.original);
+
+          if (info.buttonId == 0) {
+            // for testing we assign the first tile of the selectedTiles
+            var tilemapTile = new TilemapTile(selectedTiles[0].frame);
+            tiles[index] = tilemapTile;
+          } else {
+            tiles[index] = 0;
+          }
+
+          layerData.tiles = tiles;
+
+          var layer = tilemap.layer(layerName);
+          if (layer != null) layer.contentDirty = true;
+        }
+      }
+    }
   }
 
   public function update(dt: Float) {
