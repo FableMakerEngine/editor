230eb1dcfefe957d6730e4360dcc53fbe01aea98
inc0der <inc0der.dev@gmail.com>
2023-09-12 23:28:25 -0300

    add layer list component

---
+++ src/components/LayerList.hx
@@ -0,0 +1,10 @@
+package components;
+
+import haxe.ui.containers.ListView;
+
+@:build(haxe.ui.macros.ComponentMacros.ComponentMacros.build('../../assets/main/layer-list.xml'))
+class LayerList extends ListView {
+  public function new() {
+    super();
+  }
+}
